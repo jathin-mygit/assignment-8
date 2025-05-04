c6cdcf5b0050b502a26130eea6472cbbe9a34b71
jathin <jathin814@gmail.com>
2025-05-04 10:54:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended : true}))
+app.use(express.static("public"))
+const port = 4000
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/todo")
+
+const trySchema = new mongoose.Schema({
+    name : String
+})
+
+const item  = mongoose.model("task",trySchema)
+const todo = new item({
+    name : "create todo"
+})
+const todo1 = new item({
+    name : "delete todo"
+})
+const todo2 = new item({
+    name : "practice dsa"
+})
+const todo3 = new item({
+    name : "practice mern"
+})
+//todo.save()
+//todo1.save()
+//todo2.save()
+//todo3.save()
+
+app.get('/', async function(req, res){
+    try {
+        const foundItems = await item.find({});
+        res.render("list", { ejex: foundItems });
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+app.post('/', function(req, res){
+    const itemName = req.body.ele1
+    const todo4 = new item({
+        name : itemName
+    })  
+    todo4.save()
+    res.redirect('/')
+})
+
+app.post('/delete', async function(req, res){
+    const checked = req.body.checkbox1;
+    try {
+        await item.findByIdAndDelete(checked);
+    } catch (err) {
+        console.error(err);
+    }
+    res.redirect('/');
+});
+
+app.listen(port, function(){
+    console.log(`http://localhost:${port}`)
+})
